b4fcf638d7105c90113223eb6b2c03a324a32fa6
Lars Lippert Ovesen <au556902@uni.au.dk>
2018-04-05 11:23:48 +0200

    Added DateTimeConverter and Unit Test Project

---
+++ AutomatedTomatoMasher.library/DateTimeBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedTomatoMasher.library
+{
+    public class DateTimeBuilder : IDateTimeBuilder
+    {
+        public DateTime Build(string date)
+        {
+            //DateTime _date = DateTime.Parse(date);
+            DateTime _date =
+                DateTime.ParseExact(date, "yyyymmddhhmmssfff", CultureInfo.InvariantCulture);
+            return _date;
+        }
+    }
+}
