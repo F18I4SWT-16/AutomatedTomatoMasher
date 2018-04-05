98da095f6e31c8c7cb889203753eaf234b668dde
2takker <2takker@gmail.com>
2018-04-05 10:42:41 +0200

    Added interfaces

---
+++ AutomatedTomatoMasher.library/IDateTimeBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedTomatoMasher.library
+{
+    public interface IDateTimeBuilder
+    {
+        DateTime Build(string date);
+    }
+}
