16fb4ad116e1b73eed8fef05e951a87146a0eb88
Christian Thing Knudsen <christian.thing.knudsen@post.au.dk>
2018-04-05 11:41:29 +0200

    TransponderDecoderUnitTest

---
+++ AutomatedTomatoMasher.Test.Unit/TransponderDecoderTestUnit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomatedTomatoMasher.library;
+using NSubstitute;
+using NSubstitute.Exceptions;
+using NUnit.Framework;
+using TransponderReceiver;
+
+namespace AutomatedTomatoMasher.Test.Unit
+{
+    [TestFixture]
+    class TransponderDecoderTestUnit
+    {
+        private TransponderDecoder _uut;
+        private ITransponderReceiver _transponderReceiver;
+        private IDateTimeBuilder _dateTimeBuilder;
+        [SetUp]
+        public void SetUp()
+        {
+            _transponderReceiver = Substitute.For<ITransponderReceiver>();
+            _dateTimeBuilder = Substitute.For<IDateTimeBuilder>();
+            _uut = new TransponderDecoder(_transponderReceiver,_dateTimeBuilder);
+        }
+
+        [Test]
+        public void TransponderDecoder_MakeTrack_TrackObjectIsLoaded()
+        {
+            
+        }
+
+    }
+}
