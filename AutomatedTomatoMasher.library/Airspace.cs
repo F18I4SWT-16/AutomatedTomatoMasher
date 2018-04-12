5ec1d35ebbd32ab46dd017582a064587fdd04e1a
daniel-noerbygaard <daniel-noerbygaard@outlook.dk>
2018-04-12 11:37:44 +0200

    Super sweet changes

---
+++ AutomatedTomatoMasher.library/Airspace.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedTomatoMasher.library
+{
+    class Airspace
+    {
+        public Corner NorthEast { get; set; }
+        public Corner SouthWest { get; set; }
+        public int MaxAltitude { get; set; }
+        public int MinAltitude { get; set; }
+    }
+}
---
+++ AutomatedTomatoMasher.library/Interface/IVelocityCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomatedTomatoMasher.library.DTO;
+
+namespace AutomatedTomatoMasher.library.Interface
+{
+    interface IVelocityCalculator
+    {
+        double Calculate(List<Track> trackList);
+    }
+}
