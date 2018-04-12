fc413ca2d8e0d1c5a88640fc8fc0af2a40bb40a4
Christian Thing Knudsen <christian.thing.knudsen@post.au.dk>
2018-04-12 11:43:21 +0200

    SeperationEvent

---
+++ AutomatedTomatoMasher.library/Event/SeperationEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomatedTomatoMasher.library.DTO;
+
+namespace AutomatedTomatoMasher.library.Event
+{
+    public class SeperationEventArgs : EventArgs
+    {
+        public List<Track> TrackList { get; }
+
+        public SeperationEventArgs(List<Track> trackList)
+        {
+            TrackList = trackList;
+        }
+    }
+}
