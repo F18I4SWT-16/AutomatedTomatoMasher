c9b718ad7b6e3b2663913d2160a0201c90f8cce9
2takker <2takker@gmail.com>
2018-04-19 10:05:00 +0200

    Created Seperation Event

---
+++ AutomatedTomatoMasher.library/Event/SeperationEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomatedTomatoMasher.library.DTO;
+
+namespace AutomatedTomatoMasher.library.Event
+{
+    public class SeperationEventArgs : EventArgs
+    {
+        public List<Track> Tracks { get; }
+
+        public SeperationEventArgs(List<Track> tracks)
+        {
+            Tracks = tracks;
+        }
+    }
+}
