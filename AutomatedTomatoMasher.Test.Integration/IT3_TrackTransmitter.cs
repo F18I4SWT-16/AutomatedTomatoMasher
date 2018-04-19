29c190629f745afdf5d3b0f607ae8c016ab1a390
2takker <2takker@gmail.com>
2018-04-19 15:01:21 +0200

    Finished IT3

--- AutomatedTomatoMasher.Test.Integration/IT3_TrackTransmitter.cs
+++ AutomatedTomatoMasher.Test.Integration/IT3_TrackTransmitter.cs
@@ -34,16 +34,48 @@ namespace AutomatedTomatoMasher.Test.Integration
 
             _trackReciever = new TrackReciever(_transponderReceiver, _trackObjectifier, _uut);
 
-            _dateTimeBuilder.Build("20151006213456000").Returns(new DateTime(2015, 10, 06, 21, 34, 56));
             _list = new List<string> { "ATR423;39045;12932;14000;20151006213456000" };
 
             _uut.TrackReady += (o, args) => { _recievedTracks = args.TrackList; };
+
+            // Act
+            _transponderReceiver.TransponderDataReady +=
+                Raise.EventWith(new RawTransponderDataEventArgs(_list));
         }
 
         [Test]
-        public void TrackTransmitter_()
+        public void TrackTransmitter_Transmit_CorrectTagRecieved()
         {
-            
+            // Assert
+            Assert.That(_recievedTracks[0].Tag, Is.EqualTo("ATR423"));
+        }
+
+        [Test]
+        public void TrackTransmitter_Transmit_CorrectXRecieved()
+        {
+            // Assert
+            Assert.That(_recievedTracks[0].X, Is.EqualTo(39045));
+        }
+
+        [Test]
+        public void TrackTransmitter_Transmit_CorrectYRecieved()
+        {
+            // Assert
+            Assert.That(_recievedTracks[0].Y, Is.EqualTo(12932));
+        }
+
+        [Test]
+        public void TrackTransmitter_Transmit_CorrectAltitudeRecieved()
+        {
+            // Assert
+            Assert.That(_recievedTracks[0].Altitude, Is.EqualTo(14000));
+        }
+
+        [Test]
+        public void TrackTransmitter_Transmit_CorrectTimestampRecieved()
+        {
+            // Assert
+            Assert.That(_recievedTracks[0].TimeStamp, Is.EqualTo(new DateTime(2015,10,6,21,34,56)));
         }
 
     }
