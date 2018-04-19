9ff3066e6ebde171f45bc69a33f7f7512cf7844e
daniel-noerbygaard <daniel-noerbygaard@outlook.dk>
2018-04-19 12:13:14 +0200

    IT2 djone

--- AutomatedTomatoMasher.Test.Integration/IT1_TrackReciever.cs
+++ AutomatedTomatoMasher.Test.Integration/IT1_TrackReciever.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutomatedTomatoMasher.library;
+using AutomatedTomatoMasher.library.DTO;
 using AutomatedTomatoMasher.library.Interface;
+using NSubstitute;
 using NUnit.Framework;
+using TransponderReceiver;
 
 
 namespace AutomatedTomatoMasher.Test.Integration
@@ -16,18 +19,64 @@ namespace AutomatedTomatoMasher.Test.Integration
 
         private TrackReciever _trackReciever;
         private TrackObjectifier _uut;
-        private ITransponderReciever;
+        private ITransponderReceiver _transponderReceiver;
         private ITrackTransmitter _trackTransmitter;
         private IDateTimeBuilder _dateTimeBuilder;
+        private List<string> _list;
 
         [SetUp]
         public void SetUp()
         {
-            _trackReciever = new TrackReciever();
-            
+            _transponderReceiver = Substitute.For<ITransponderReceiver>();
+            _trackTransmitter = Substitute.For<ITrackTransmitter>();
+            _dateTimeBuilder = Substitute.For<IDateTimeBuilder>();
+
+            _uut = new TrackObjectifier(_dateTimeBuilder);
+
+            _trackReciever = new TrackReciever(_transponderReceiver, _uut ,_trackTransmitter);
+
+            _dateTimeBuilder.Build("20151006213456000").Returns(new DateTime(2015, 10, 06, 21, 34, 56));
+             _list = new List<string> { "ATR423;39045;12932;14000;20151006213456000" };
         }
 
+        [Test]
+        public void TrackObjectifier_Objectify_CorrectXReturned()
+        {
+            //Act & Assert
+            Assert.That(_uut.Objectify(_list)[0].X, Is.EqualTo(39045));
+        }
 
+        [Test]
+
+        public void TrackObjectifier_Objectify_CorrectYReturned()
+        {
+            //Act & Assert
+            Assert.That(_uut.Objectify(_list)[0].Y, Is.EqualTo(12932));
+        }
+
+        [Test]
+
+        public void TrackObjectifier_Objectify_CorrectTagReturned()
+        {
+            //Act & Assert
+            Assert.That(_uut.Objectify(_list)[0].Tag, Is.EqualTo("ATR423"));
+        }
+
+        [Test]
+
+        public void TrackObjectifier_Objectify_CorrectAltitudeReturned()
+        {
+            //Act & Assert
+            Assert.That(_uut.Objectify(_list)[0].Altitude, Is.EqualTo(14000));
+        }
+
+        [Test]
+
+        public void TrackObjectifier_Objectify_CorrectTimeStampReturned()
+        {
+            //Act & Assert
+            Assert.That(_uut.Objectify(_list)[0].TimeStamp, Is.EqualTo(new DateTime(2015, 10, 06, 21, 34, 56)));
+        }
 
     }
 }
