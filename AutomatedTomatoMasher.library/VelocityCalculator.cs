12ef9356927af3976460803f3ec169c00594bbcf
Christian Thing Knudsen <christian.thing.knudsen@post.au.dk>
2018-04-19 11:11:30 +0200

    AtmUnittest and VelocityCalculator class

---
+++ AutomatedTomatoMasher.library/VelocityCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomatedTomatoMasher.library.DTO;
+using AutomatedTomatoMasher.library.Interface;
+
+namespace AutomatedTomatoMasher.library
+{
+    class VelocityCalculator: IVelocityCalculator
+    {
+        public double Calculate(List<Track> trackList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
