69e4850325b065615ce0bd2fc3ca28ee8d7b05c9
Lars Lippert Ovesen <au556902@uni.au.dk>
2018-04-19 13:37:10 +0200

    Unit test for Velocity

--- AutomatedTomatoMasher.Test.Unit/VelocityCalculatorTestUnit.cs
+++ AutomatedTomatoMasher.Test.Unit/VelocityCalculatorTestUnit.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutomatedTomatoMasher.library;
+using AutomatedTomatoMasher.library.DTO;
 using NSubstitute;
 using NUnit;
 using NUnit.Framework;
@@ -22,7 +23,32 @@ namespace AutomatedTomatoMasher.Test.Unit
             _uut = new VelocityCalculator();
         }
 
-        [Test]
-        public void 
+        [TestCase(100,0,0)]
+        public void Calculator_MovementInOneSecound_ReturnsCorrectVelocity(int X, int Y, int altitude)
+        {
+            //Arrange
+            var trackList = new List<Track>() {
+                new Track()
+                {
+                    Tag = "ATR423",
+                    X = 10000,
+                    Y = 10000,
+                    Altitude = 500,
+                    TimeStamp = new DateTime(2018, 01, 01, 00, 00, 00, 000)
+                },
+                new Track()
+                {
+                    Tag = "ATR423",
+                    X = 10000+X,
+                    Y = 10000+Y,
+                    Altitude = 500+altitude,
+                    TimeStamp = new DateTime(2018, 01, 01, 00, 01, 00, 000)
+                }
+            };
+
+            //Act and Assert
+            Assert.That(_uut.Calculate(trackList), Is.EqualTo(100));
+            
+        }
     }
 }
