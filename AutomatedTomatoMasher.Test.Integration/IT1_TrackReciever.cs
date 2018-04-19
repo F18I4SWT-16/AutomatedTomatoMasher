647e80de3b2cda7910e03be7d6223c28ecfb7ecb
daniel-noerbygaard <daniel-noerbygaard@outlook.dk>
2018-04-19 09:49:11 +0200

    Opstart på IT

---
+++ AutomatedTomatoMasher.Test.Integration/IT1_TrackReciever.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomatedTomatoMasher.library;
+using AutomatedTomatoMasher.library.Interface;
+using NUnit.Framework;
+
+
+namespace AutomatedTomatoMasher.Test.Integration
+{
+    [TestFixture]
+    public class IT1_TrackReciever
+    {
+
+        private TrackReciever _trackReciever;
+        private TrackObjectifier _uut;
+        private ITransponderReciever;
+        private ITrackTransmitter _trackTransmitter;
+        private IDateTimeBuilder _dateTimeBuilder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _trackReciever = new TrackReciever();
+            
+        }
+
+
+
+    }
+}
