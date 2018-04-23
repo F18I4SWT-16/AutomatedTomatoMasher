8085276ef2cbc780781169b30fe1b22a6b70eb16
Christian Thing Knudsen <christian.thing.knudsen@post.au.dk>
2018-04-23 20:48:26 +0200

    Fixed SeperationEventLogger

--- AutomatedTomatoMasher.library/SeperationEventLogger.cs
+++ AutomatedTomatoMasher.library/SeperationEventLogger.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using AutomatedTomatoMasher.library.DTO;
+using AutomatedTomatoMasher.library.Event;
 using AutomatedTomatoMasher.library.Interface;
 
 namespace AutomatedTomatoMasher.library
@@ -13,24 +14,26 @@ namespace AutomatedTomatoMasher.library
     public class SeperationEventLogger: ISeperationEventLogger
     {
         private readonly IOutput _output;
-        
-        public SeperationEventLogger(IOutput output)
+
+        public SeperationEventLogger(IOutput output, ISeperationEventChecker seperationEventChecker)
         {
             _output = output;
+            seperationEventChecker.SeperationEvent += Log;
         }
 
-        public void Log(List<Track> tracks)
+        private void Log(object sender, SeperationEventArgs args)
         {
-            using (FileStream output = new FileStream("SeperationLogFile.txt", FileMode.Create, FileAccess.Write))
+            string fullDirectory = @"...\...\...\";
+            using (FileStream output = new FileStream(fullDirectory + "/SeperationLogFile.txt", FileMode.Append, FileAccess.Write))
             using (StreamWriter fileWriter = new StreamWriter(output))
             {
-                fileWriter.WriteLine("Flights in Conflict: " + tracks[0].Tag + ", " + tracks[1].Tag + "\nTime stamp of conflict: " +
-                                     tracks[0].Timestamp.Year + "/" + tracks[0].Timestamp.Month + "/" + tracks[0].Timestamp.Day +
-                                     ", at " + tracks[0].Timestamp.Hour + ":" + tracks[0].Timestamp.Minute + ":" +
-                                     tracks[0].Timestamp.Second + " and " + tracks[0].Timestamp.Millisecond + " milliseconds\n");
+                fileWriter.WriteLine("Flights in Conflict: " + args.Tracks[0].Tag + ", " + args.Tracks[1].Tag + "\nTime stamp of conflict: " +
+                                     args.Tracks[0].Timestamp.Year + "/" + args.Tracks[0].Timestamp.Month + "/" + args.Tracks[0].Timestamp.Day +
+                                     ", at " + args.Tracks[0].Timestamp.Hour + ":" + args.Tracks[0].Timestamp.Minute + ":" +
+                                     args.Tracks[0].Timestamp.Second + " and " + args.Tracks[0].Timestamp.Millisecond + " milliseconds");
                 fileWriter.Close();
             }
-            _output.Write(tracks);
+            _output.Write(args.Tracks);
         }
     }
 }
